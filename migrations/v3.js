86b6e791a8a18a5d60204b8cfc9e8a7a1a69eb3c
Brad Simpson <brad.simpson@kineo.com>
2025-02-11 15:28:47 -0700

    v3 scripts

--- migrations/v3.js
+++ migrations/v3.js
@@ -1,5 +1,5 @@
-import { describe, whereContent, whereFromPlugin, mutateContent, checkContent, updatePlugin } from 'adapt-migrations';
-// import _ from 'lodash';
+import { describe, whereFromPlugin, mutateContent, checkContent, updatePlugin } from 'adapt-migrations';
+import _ from 'lodash';
 
 describe('Navigation Logo - v2.1.0 to v3.0.1', async () => {
   let course, courseNavLogoGlobals;
@@ -7,11 +7,11 @@ describe('Navigation Logo - v2.1.0 to v3.0.1', async () => {
   mutateContent('Navigation Logo - add globals if missing', async (content) => {
     course = content.find(({ _type }) => _type === 'course');
     if (!_.has(course, '_globals._extensions._navigationLogo')) _.set(course, '_globals._extensions._navigationLogo', {});
-    courseNavLogoGlobals = course._globals._extensions._navLogo;
+    courseNavLogoGlobals = course._globals._extensions._navigationLogo;
     return true;
   });
   mutateContent('Navigation Logo - add globals _navOrder', async (content) => {
-    courseNavLogoGlobals._navOrder = 1;
+    _.set(courseNavLogoGlobals, '_navOrder', 1);
     return true;
   });
   checkContent('Navigation Logo - check globals _navOrder attribute', async content => {
@@ -25,19 +25,26 @@ describe('Navigation Logo - v2.1.0 to v3.0.1', async () => {
   updatePlugin('Navigation Logo - update to v3.0.1', { name: 'adapt-navigationLogo', version: '3.0.1', framework: '>=5.0.0' });
 });
 
-// describe('Navigation Logo - v to v3.3.0', async () => {
-//   let graphics, course, courseNavLogoGlobals;
-//   whereFromPlugin('Navigation Logo - from v', { name: 'adapt-navigationLogo', version: '<3.3.0' });
-
-//   // mutateContent('Graphic - add globals if missing', async (content) => {
-//   //   course = content.find(({ _type }) => _type === 'course');
-//   //   if (!_.has(course, '_globals._components._graphic')) _.set(course, '_globals._components._graphic', {});
-//   //   courseNavLogoGlobals = course._globals._components._graphic;
-//   //   return true;
-//   // });
-//   // checkContent('Graphic - check globals _graphic attribute', async content => {
-//   //   if (courseNavLogoGlobals === undefined) throw new Error('Graphic - globals _graphic invalid');
-//   //   return true;
-//   // });
-//   updatePlugin('Navigation Logo - update to v3.3.0', { name: 'adapt-navigationLogo', version: '3.3.0', framework: '>=5.30.3' });
-// });
+describe('Navigation Logo - v3.0.1 to v3.0.3', async () => {
+  let course, courseNavLogo;
+  whereFromPlugin('Navigation Logo - from v3.0.1', { name: 'adapt-navigationLogo', version: '<3.0.3' });
+  mutateContent('Navigation Logo - add course _navigationLogo if missing', async (content) => {
+    course = content.find(({ _type }) => _type === 'course');
+    if (!_.has(course, '_navigationLogo')) _.set(course, '_navigationLogo', {});
+    courseNavLogo = course._navigationLogo;
+    return true;
+  });
+  mutateContent('Navigation Logo - add course _navigationLogo._isHidden', async (content) => {
+    courseNavLogo._isHidden = false;
+    return true;
+  });
+  checkContent('Navigation Logo - check course _navigationLogo object', async content => {
+    if (courseNavLogo === undefined) throw new Error('Navigation Logo - course _navigationLogo invalid');
+    return true;
+  });
+  checkContent('Navigation Logo - check course _navigationLogo._isHidden', async content => {
+    if (courseNavLogo._isHidden !== false) throw new Error('Navigation Logo - course _navigationLogo._isHidden invalid');
+    return true;
+  });
+  updatePlugin('Navigation Logo - update to v3.0.3', { name: 'adapt-navigationLogo', version: '3.0.3', framework: '>=5.0.0' });
+});
--- migrations/v4.js
+++ migrations/v4.js
@@ -1,22 +1,6 @@
-import { describe, whereContent, whereFromPlugin, mutateContent, checkContent, updatePlugin } from 'adapt-migrations';
+// import { describe, whereContent, whereFromPlugin, mutateContent, checkContent, updatePlugin } from 'adapt-migrations';
 // import _ from 'lodash';
 
-describe('Navigation Logo - v3.3.0 to v4.0.0', async () => {
-  // let graphics, course, courseNavLogoGlobals;
-  // whereFromPlugin('Graphic - from v3.3.0', { name: 'adapt-navigationLogo', version: '<4.0.0' });
-  // whereContent('Graphic - where graphic', async content => {
-  //   graphics = content.filter(({ _component }) => _component === 'graphic');
-  //   return graphics.length;
-  // });
-  // mutateContent('Graphic - add globals if missing', async (content) => {
-  //   course = content.find(({ _type }) => _type === 'course');
-  //   if (!_.has(course, '_globals._components._graphic')) _.set(course, '_globals._components._graphic', {});
-  //   courseNavLogoGlobals = course._globals._components._graphic;
-  //   return true;
-  // });
-  // checkContent('Graphic - check globals _graphic attribute', async content => {
-  //   if (courseNavLogoGlobals === undefined) throw new Error('Graphic - globals _graphic invalid');
-  //   return true;
-  // });
-  updatePlugin('Navigation Logo - update to v4.0.0', { name: 'adapt-navigationLogo', version: '4.0.0', framework: '>=5.30.3' });
-});
+// describe('Navigation Logo - v3.3.0 to v4.0.0', async () => {
+//   updatePlugin('Navigation Logo - update to v4.0.0', { name: 'adapt-navigationLogo', version: '4.0.0', framework: '>=5.30.3' });
+// });
